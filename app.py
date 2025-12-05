46a2d4a858f46d8617550baf96d49ab34b11a723
Projavax <156926246+Projavax@users.noreply.github.com>
2025-12-05 08:26:38 +0000

    minor changes

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # This is what the user sees when they visit the website
     return "Hello NatWest! This app is running inside a Docker container."
 
 if __name__ == '__main__':
